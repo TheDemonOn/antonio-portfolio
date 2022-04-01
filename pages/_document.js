8a542731ff38906ad58f8f3b7f687d8f9ad5e0c8
Antonio Zamora <zamora88875@gmail.com>
2022-04-01 17:05:56 -0400

    Initial setup.

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<meta name="description" content="Antonio's portfolio site" />
+				<meta name="author" content="Antonio Zamora" />
+				<meta name="keywords" content="Antonio, portfolio, front end development" />
+				{/* <meta name="theme-color" content="#262538" /> */}
+				<meta name="language" content="English" />
+				{/* <meta name="revised" content="March 22nd, 2022" /> */}
+				<meta name="HandheldFriendly" content="true" />
+
+				{/* <link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="icon" href="/favicon.svg" type="image/svg+xml" /> */}
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
