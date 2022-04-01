bce3a5461832405c1f9bd112008442b7882b52a4
Antonio Zamora <zamora88875@gmail.com>
2022-04-01 17:07:39 -0400

    Added a custom hook: useInterval and used it.

---
+++ hooks/useInterval.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react'
+
+// Thanks to Dan Abramov's explanation and implementation of setInterval in hooks: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+
+export default function useInterval(callback, delay) {
+	// First we create the ref which will store the function we want to pass into it.
+	const ref = useRef()
+
+	// As is usual with a useRef formula: the .current is set to the value we want to know the value of in between renders.
+	useEffect(() => {
+		ref.current = callback
+	}, [callback])
+
+	// This is where it gets more complicated. I need to understand the whys a bit better to explain it myself.
+	useEffect(() => {
+		// This function is used to access the previous callback, which is a placeholder for the contents of our setInterval.
+		const tick = () => {
+			ref.current()
+		}
+		let id = setInterval(tick, delay)
+		return () => {
+			clearInterval(id)
+		}
+	}, [delay])
+}
