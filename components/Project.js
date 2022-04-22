a6edd2489f96896c26f7def864d2ec8cf38c5226
Antonio Zamora <zamora88875@gmail.com>
2022-04-22 12:00:25 -0400

    Added on hover to change the preview images.

--- components/Project.js
+++ components/Project.js
@@ -1,13 +1,39 @@
 import React, { useEffect } from 'react'
 import styles from '../styles/Project.module.css'
+import Image from 'next/image'
 
 const Project = React.forwardRef(
-	({ title, color, linkName, link, img, content, position }, ref) => {
+	({ title, linkName, link, content, position, src, srcHover, alt = 'No alt here.' }, ref) => {
+		useEffect(() => {
+			let picture = document.getElementById('picture' + position)
+			let hover = document.getElementById('hover' + position)
+
+			const swapPicture = () => {
+				picture.style.display = 'none'
+				hover.style.display = 'unset'
+			}
+			picture.addEventListener('mouseenter', swapPicture)
+
+			const swapBack = () => {
+				picture.style.display = 'unset'
+				hover.style.display = 'none'
+			}
+			hover.addEventListener('mouseleave', swapBack)
+		}, [])
+
 		return (
 			<>
 				<div className={styles.fullSection} id={position} ref={ref}>
 					<div className={styles.right}>
-						<div className={styles.pictureFrame}></div>
+						<div className={styles.pictureFrame} id={'picture' + position}>
+							<Image src={src} alt={alt} />
+						</div>
+
+						<div className={styles.pictureFrameHover} id={'hover' + position}>
+							<a href={link}>
+								<Image src={srcHover} alt={alt} loading="eager" priority="true" />
+							</a>
+						</div>
 					</div>
 					<div className={styles.left}>
 						<div className={styles.project}>
