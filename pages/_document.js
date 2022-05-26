60136afb7bc3e9220670eeea97db7d580ee55e5a
Antonio Zamora <zamora88875@gmail.com>
2022-05-26 14:22:22 -0400

    Added favicons.

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,12 @@ export default function Document() {
 				<meta name="HandheldFriendly" content="true" />
 				<link rel="stylesheet" href="https://use.typekit.net/hah6qsj.css" />
 
-				{/* <link rel="icon" href="/favicon.ico" sizes="any" />
-				<link rel="icon" href="/favicon.svg" type="image/svg+xml" /> */}
+				<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+				<link rel="manifest" href="/images/site.webmanifest" />
+				<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="msapplication-TileColor" content="#da532c" />
 			</Head>
 			<body>
 				<Main />
